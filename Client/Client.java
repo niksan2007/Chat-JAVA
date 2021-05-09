6a656876e7c13f8afdfb21d653538a0a1ee2d136
c4l4t <kozlov.07.93@mail.ru>
2021-05-09 12:54:01 +0700

    3

--- Client/Client.java
+++ Client/Client.java
@@ -19,21 +19,35 @@ public class Client {
         isConnect = connect;
     }
 
-    //точка входа в клиентское приложение
-    public static void main(String[] args) {
-        Client client = new Client();
-        model = new ModelGuiClient();
-        gui = new ViewGuiClient(client);
-        gui.initFrameClient();
-        while (true) {
-            if (client.isConnect()) {
-                client.nameUserRegistration();
-                client.receiveMessageFromServer();
-                client.setConnect(false);
-            }
-        }
+   public class ViewGuiServer {
+    private JFrame frame = new JFrame("Запуск сервера");
+    private JTextArea dialogWindow = new JTextArea(10, 40);
+    private JButton buttonStartServer = new JButton("Запустить сервер");
+    private JButton buttonStopServer = new JButton("Остановить сервер");
+    private JPanel panelButtons = new JPanel();
+    private final Server server;
+
+    public ViewGuiServer(Server server) {
+        this.server = server;
     }
 
+    //метод инициализации графического интерфейса приложения сервера
+    protected void initFrameServer() {
+        dialogWindow.setEditable(false);
+        dialogWindow.setLineWrap(true);  //автоматический перенос строки в JTextArea
+        frame.add(new JScrollPane(dialogWindow), BorderLayout.CENTER);
+        panelButtons.add(buttonStartServer);
+        panelButtons.add(buttonStopServer);
+        frame.add(panelButtons, BorderLayout.SOUTH);
+        frame.pack();
+        frame.setLocationRelativeTo(null); // при запуске отображает окно по центру экрана
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        //класс обработки события при закрытии окна приложения Сервера
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                server.stopServer();
+                System.exit(0);
     //метод подключения клиента  серверу
     protected void connectToServer() {
         //если клиент не подключен  сервере то..
